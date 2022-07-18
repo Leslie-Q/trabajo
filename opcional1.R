428116ff2a06e546b57e189ac26430e456fba91c
Leslie-Q <109159675+Leslie-Q@users.noreply.github.com>
2022-07-18 10:44:26 -0500

    Shiny

---
+++ opcional1.R
@@ -0,0 +1,22 @@
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+
+
+library(readxl)
+datos<-read_excel("opcional.xlsx")
+datos=as.data.frame(datos)
+names(datos)
+fosforo<-factor(datos[,1])
+riego<-factor(datos[,2])
+variedad<-factor(datos[,3])
+y<-datos[,4]
+
+
+mod1<-lm(y~variedad+fosforo+riego);mod1
+shapiro.test(rstandard(mod1))
+round( 0.05566,4)
+
+#ANVA
+summary(aov(mod1))
